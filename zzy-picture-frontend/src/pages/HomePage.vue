9aee25f1e57e1cdde2a26a8b69179c1dbcbe8e80
LovePluto520 <strangeman613911@gmail.com>
2025-07-27 14:46:36 +0800

    第一版：项目初始化

---
+++ zzy-picture-frontend/src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="homePage">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const msg = "欢迎来到小羊云图库，你可以找到你喜欢的图库";
+</script>
+
+<style scoped>
+#homePage {
+
+}
+</style>
