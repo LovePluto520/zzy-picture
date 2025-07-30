c8d9b986ecf5e75bfab9c996c5becf3563db9a15
LovePluto520 <strangeman613911@gmail.com>
2025-07-30 16:32:31 +0800

    第二版：用户模块前端开发

--- zzy-picture-frontend/src/router/index.ts
+++ zzy-picture-frontend/src/router/index.ts
@@ -1,14 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import HomePage from "@/pages/HomePage.vue";
+import UserManagePage from "@/pages/admin/UserManagePage.vue";
+import UserRegisterPage from "@/pages/user/UserRegisterPage.vue";
+import UserLoginPage from "@/pages/user/UserLoginPage.vue";
+import ACCESS_ENUM from "@/access/accessEnum.ts";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: '主页',
       component: HomePage,
+      meta:{
+        hideInMenu: false,
+      },
+    },
+    {
+      path: '/user/login',
+      name: '用户登录',
+      component: UserLoginPage,
+      meta:{
+        hideInMenu: true,
+      },
+    },
+    {
+      path: '/user/register',
+      name: '用户注册',
+      component: UserRegisterPage,
+      meta:{
+        hideInMenu: true,
+      },
+    },
+    {
+      path: '/admin/userManage',
+      name: 'adminUserManage',
+      component: UserManagePage,
+      meta: {
+        access: ACCESS_ENUM.ADMIN,
+      },
     },
     {
       path: '/about',
@@ -17,8 +48,12 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
-    },
+      meta: {
+        hideInMenu: false,
+      },
+    }
   ],
+
 })
 
 export default router
